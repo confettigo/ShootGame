473a7a501963fa0bfb338d4a68aab4c303ffac57
daurek <ilyassgame@gmail.com>
2025-01-03 01:31:26 +0100

    Basic Movement

---
+++ Scripts/Player/Movement.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed = 200
+
+func get_input():
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
