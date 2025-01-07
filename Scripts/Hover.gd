04eefe4f032b5de734b6867bef29774cf66a2f07
daurek <ilyassgame@gmail.com>
2025-01-07 03:12:18 +0100

    Shadow Tests on interface and game

--- Scripts/Hover.gd
+++ Scripts/Hover.gd
@@ -1,5 +1,8 @@
 extends Sprite2D
 
+@export var offsetHover : float
+@export var signE : float = 1
+
 func _process(_delta: float) -> void:
-	position.y = round(sin(Time.get_ticks_msec() * 0.005) * 2) - 5
+	position.y = round(sin(Time.get_ticks_msec() * signE * 0.005) * 2) - offsetHover
 	
--- Scripts/Pickup.gd
+++ Scripts/Pickup.gd
@@ -2,9 +2,11 @@ extends Node
 
 @export var weapon : Weapon
 @export var sprite : Sprite2D
+@export var shadow : Sprite2D
 
 func _ready():
 	sprite.texture = weapon.pickupSprite
+	shadow.texture = weapon.pickupSprite
 
 func _on_body_entered(body:Node2D) -> void:
 	if body.is_in_group("player"):
